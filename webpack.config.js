843b32441c26ba4b4cc2ce19d794b328605c4fe9
Hugh Morrison <hmorri32@gmail.com>
2017-01-23 17:17:48 -0700

    install babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,8 +11,12 @@ module.exports = {
   },
   module: {
     loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: "style!css!sass" }
     ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
   }
 }
