640abef1e663436565ea9597a03bd68eec75d37a
Hugh Morrison <hmorri32@gmail.com>
2017-01-28 12:19:41 -0700

    delete commented code in test js

--- test/index.js
+++ test/index.js
@@ -28,7 +28,7 @@ describe('2-Do-box test bruu', function() {
   });
 
   test.it('should allow me to add a task', function() {
-    
+
     const task = driver.findElement({id: "body-input"});
 
     task.sendKeys("brah").then(()=> { return task.getAttribute('value')}).then((value) => {
@@ -37,77 +37,3 @@ describe('2-Do-box test bruu', function() {
   });
 
 });
-
-
-  // test.it('should allow me to add a title and a description', ()=>{
-  //
-  //   const title       = driver.findElement({name: "title"});
-  //   const description = driver.findElement({name: "description"});
-  //
-  //   title.sendKeys('this is a title').then(()=> { return title.getAttribute('value')}).then((value)=> {
-  //     assert.equal(value, 'this is a title')
-  //   });
-  //   description.sendKeys('this is a description').then(()=> { return description.getAttribute('value')}).then((value)=> {
-  //     assert.equal(value, 'this is a description')
-  //   });
-  // });
-//
-//   test.it('should allow me to add an idea to the dom', ()=> {
-//
-//     const title       = driver.findElement({name: "title"});
-//     const description = driver.findElement({name: "description"});
-//     const button      = driver.findElement({name: "button"});
-//
-//     title.sendKeys('this is a title');
-//     description.sendKeys('this is a description');
-//     button.click();
-//
-//     const idea = driver.findElement({tagName: 'li'})
-//
-//     idea.getText().then((value)=> {
-//       assert.equal(value, 'this is a title\nthis is a description')
-//     });
-//   });
-//
-//   test.it("add two ideas, delete one, assert that there is one left", ()=>{
-//
-//     const title       = driver.findElement({name: "title"});
-//     const description = driver.findElement({name: "description"});
-//     const button      = driver.findElement({name: "button"});
-//
-//     title.sendKeys("this is a title");
-//     description.sendKeys('this is a description');
-//     button.click();
-//
-//     title.sendKeys("this is another title");
-//     description.sendKeys('this is another description');
-//     button.click();
-//
-//     driver.findElements({tagName: 'li'}).then((li)=> {
-//       assert.equal(li.length, 2)
-//     })
-//
-//     driver.findElement({className: "delete-idea"}).click();
-//
-//     driver.findElements({tagName: 'li'}).then((li)=> {
-//       assert.equal(li.length, 1)
-//     })
-//   })
-//
-// });
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-// //
