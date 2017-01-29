ef7c4c5325c938307049d42be68b6fd6586e4e8a
Hugh Morrison <hmorri32@gmail.com>
2017-01-29 13:43:39 -0700

    add test for default importance level

--- test/index.js
+++ test/index.js
@@ -56,7 +56,7 @@ describe('2-Do-box test bruu', function() {
     });
   });
 
-  test.it.only('should allow me to delete an idea from the DOM', function(){
+  test.it('should allow me to delete an idea from the DOM', function(){
     const title     = driver.findElement({id: "title-input"});
     const task      = driver.findElement({id: "body-input"});
     const button    = driver.findElement({id: "save-button"});
@@ -79,38 +79,21 @@ describe('2-Do-box test bruu', function() {
     driver.findElements({className: 'new-ideas'}).then((idea)=> {
       assert.equal(idea.length, 1)
     })
+  })
 
+  test.it('new idea in DOM should have a default importance level of normal ', function() {
+    const title     = driver.findElement({id: "title-input"});
+    const task      = driver.findElement({id: "body-input"});
+    const button    = driver.findElement({id: "save-button"});
 
+    title.sendKeys('this is a title')
+    task.sendKeys('this is a task')
+    button.click();
 
-
+    const quality = driver.findElement({className: "quality"})
+    quality.getText().then((value) => {
+      assert.equal(value, "normal");
+    })
   })
 
 });
-
-
-//   test.it("add two ideas, delete one, assert that there is one left", ()=>{
-//
-//     const title       = driver.findElement({name: "title"});
-//     const description = driver.findElement({name: "description"});
-//     const button      = driver.findElement({name: "button"});
-//
-//     title.sendKeys("this is a title");
-//     description.sendKeys('this is a description');
-//     button.click();
-//
-//     title.sendKeys("this is another title");
-//     description.sendKeys('this is another description');
-//     button.click();
-//
-//     driver.findElements({tagName: 'li'}).then((li)=> {
-//       assert.equal(li.length, 2)
-//     })
-//
-//     driver.findElement({className: "delete-idea"}).click();
-//
-//     driver.findElements({tagName: 'li'}).then((li)=> {
-//       assert.equal(li.length, 1)
-//     })
-//   })
-//
-// });
